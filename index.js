c0bad6ad6de1ee4de5cdbd38e040acdcfafc6802
William Wong <wwong1987@gmail.com>
2019-01-27 18:39:39 -0800

    able to fetch API but not display properly

--- index.js
+++ index.js
@@ -1,87 +1,68 @@
 /*global $*/
 'use strict';
 
-const apiKey = 'addg1a0cb0qmUfz5gHiyFUAysPuw71fz5rbqnqLL';
+// create variable for apiKey and URL
+const api_key = 'addg1a0cb0qmUfz5gHiyFUAysPuw71fz5rbqnqLL';
 const searchURL = 'https://developer.nps.gov/api/v1/parks';
 
-
-function setQueryParam(param){
-  const queryItems=Object.keys(param)
-    .map(key=>`${encodeURIComponent(key)}=${encodeURIComponent(param[key])}`);
+// create function for formating array of keys into a string
+function formatQueryParams(query){
+  const queryItems = Object.keys(query)
+    .map(key => `${key}=${query[key]}`);
+  console.log(queryItems);
   return queryItems.join('&');
 }
 
-
-function getStateParks(query, maxResults){
-  const param ={
-    api_key: apiKey,
-    q: query,
-    maxResults
+// create function for fetching API with params
+function getStateParks(searchTerm, maxResults = 10) {
+  // create query params
+  const query ={
+    searchTerm,
+    maxResults,
   };
-  const queryString = setQueryParam(param);
-  const url = searchURL + '?' + queryString;
+  const queryString = formatQueryParams(query);
+  const url = `${searchURL}?${queryString}&api_key=${api_key}`;
   // URL?q=query&maxResults=maxResults
 
   fetch(url)
-    .then(res => res.json())
-    .then(resJson => displayParkResults(resJson));
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => console.log(responseJson))
+    // displayParkResults(responseJson, maxResults)
+    .catch(err => {
+      $('#js-error-message').text('Something went wrong: ${err.message}');
+    });
 }
 
-
-
-function displayParkResults(resJson) {
+// create function for display API results
+function displayParkResults(responseJson) {
   $('results-list').empty();
   let html = '';
-  for (let i = 0; i <resJson.items.length; i++) {
-    const park = resJson;
-    console.log(park);
-    const name = park.name;
-    const description = park.description;
-    const url = park.url;
+  for (let i = 0; i <responseJson.data.length; i++) {
     $('#results-list').append(
-      `<li><h3>${name}</h3>
-        Description: ${description},
-        URL: ${url}
-      </li>`
+      `<ul>
+      <li>State: ${responseJson.data[i].states}</li><br>
+      <li>Park Name: ${responseJson.data[i].fullName}</li><br>
+      <li>Park Description: ${responseJson.data[i].description}>/li><br>
+      </ul>`
     );
     $('results-list').html(html); 
   }
 }
 
-displayParkResults();
 
-
-
+// function for watching form submission
 function watchForm() {
   $('form').submit(function(e) {
     e.preventDefault();
-    const searchTerm = $('#searchInput').val();
-    const maxResults = $('#limitSearchInput').val();
+    const searchTerm = $('#js-searchInput').val();
+    const maxResults = $('#js-limitSearchInput').val();
     getStateParks(searchTerm, maxResults);
   });
 }
 
 $(watchForm);
-
-
-// Requirements:
-// The user must be able to search for parks in one or more states.
-// The user must be able to set the max number of results, with a default of 10.
-// The search must trigger a call to NPS's API.
-// The parks in the given state must be displayed on the page. Include at least:
-// Full name
-// Description
-// Website URL
-// The user must be able to make multiple searches and see only the results for the current search.
-
-
-
-// function testWord() {
-//   console.log('test search input');
-// }
-
-// function handlingSearchInput() {
-//   $('form').on('submit', function(e) {
-//     e.preventDefault();
-//   });
-// }
