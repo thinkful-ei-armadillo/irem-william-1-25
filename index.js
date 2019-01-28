2d67ab129f06b48a9c79e66705df4031dc2ac63d
William Wong <wwong1987@gmail.com>
2019-01-28 00:05:49 -0800

    displays random info, but wont work with max results input and doesnt filter correctly

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function getStateParks(searchTerm, maxResults = 10) {
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => console.log(responseJson))
+    .then(responseJson => displayParkResults(responseJson, maxResults))
     // displayParkResults(responseJson, maxResults)
     .catch(err => {
       $('#js-error-message').text('Something went wrong: ${err.message}');
@@ -39,28 +39,28 @@ function getStateParks(searchTerm, maxResults = 10) {
 }
 
 // create function for display API results
-function displayParkResults(responseJson) {
-  $('results-list').empty();
-  let html = '';
-  for (let i = 0; i <responseJson.data.length; i++) {
+function displayParkResults(responseJson, maxResults) {
+  console.log(responseJson);
+  $('#results-list').empty();
+  for (let i = 0; i < responseJson.data.length & i < maxResults; i++) {
     $('#results-list').append(
-      `<ul>
-      <li>State: ${responseJson.data[i].states}</li><br>
-      <li>Park Name: ${responseJson.data[i].fullName}</li><br>
-      <li>Park Description: ${responseJson.data[i].description}>/li><br>
+      `<h2>Search Results</h2><ul>
+      <li>State: ${responseJson.data[i].states}</li>
+      <p>Park Name: ${responseJson.data[i].fullName}</p>
+      <p>Park Description: ${responseJson.data[i].description}</p><br>
       </ul>`
     );
-    $('results-list').html(html); 
+    // $('#results-list').removeClass('hidden');
   }
 }
 
 
 // function for watching form submission
 function watchForm() {
-  $('form').submit(function(e) {
-    e.preventDefault();
-    const searchTerm = $('#js-searchInput').val();
-    const maxResults = $('#js-limitSearchInput').val();
+  $('form').submit(function(event) {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    const maxResults = $('#js-max-results').val();
     getStateParks(searchTerm, maxResults);
   });
 }
